95f48a84c0f3545b3b91652292555f09ecf7ba34
Loaf the Computerphile <53319491+loafthecomputerphile@users.noreply.github.com>
2026-03-04 16:56:42 -0500

    starting random line func

--- BrainrotGuesser.asm
+++ BrainrotGuesser.asm
@@ -2,6 +2,7 @@
 INCLUDE Irvine32.inc
 
 .data
+    databaseName    BYTE "brainrot_database.txt", 0
     filename_copy   BYTE 256 DUP(0)          ; stored filename for LoadFileLine
     line_buffer     BYTE 1024 DUP(0)         ; holds one line (null‑terminated)
     file_buffer     BYTE 4096 DUP(0)         ; temporary read buffer
@@ -11,7 +12,30 @@ INCLUDE Irvine32.inc
     current_line    DWORD ?                   ; line counter while reading
     is_copying      BYTE ?                     ; flag: 1 = copying target line
 
+    fileHandle       DWORD ?
+    bytesRead        DWORD ?
+
 .code
+
+; returns: EAX = random line index (0-based)
+GetRandomFileLine PROC
+    ; 1. Open the file to count total lines
+    INVOKE CreateFile, ADDR databaseName, GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0
+    mov fileHandle, eax
+    cmp eax, INVALID_HANDLE_VALUE
+    je  Fail
+
+    mov current_line, 0
+
+    CountLoop:
+        INVOKE ReadFile, fileHandle, ADDR file_buffer, 4096, ADDR bytesRead, 0
+        cmp bytesRead, 0
+        je  EndCount
+        mov ecx, bytesRead
+        mov esi, OFFSET file_buffer
+    
+GetRandomFileLine ENDP
+
 main PROC
 
 	mov	eax,val1			; start with 10000h
@@ -22,7 +46,5 @@ main PROC
 
 	exit
 main ENDP
-
-
 
 END main
